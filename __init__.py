1609883e3ac76cff9ee3fedb9522a3b1f9f1d87a
Alan Burns <alan.burns@me.com>
2025-09-24 11:33:52 -0500

    Fixed node import bug

--- __init__.py
+++ __init__.py
@@ -1,54 +1,40 @@
-from .linxutil.multipurpose_configuration import (
-    NODE_CLASS_MAPPINGS as MPC_MAP,
-    NODE_DISPLAY_NAME_MAPPINGS as MPC_NAME,
-)
-from .linxutil.auto_color_match import (
-    NODE_CLASS_MAPPINGS as ACM_MAP,
-    NODE_DISPLAY_NAME_MAPPINGS as ACM_NAME,
-)
-from .linxutil.remove_extension import (
-    NODE_CLASS_MAPPINGS as STRIP_EXT_MAP,
-    NODE_DISPLAY_NAME_MAPPINGS as STRIP_EXT_NAME,
-)
-
-# NEW
-from .linxutil.auto_crop_borders import (
-    NODE_CLASS_MAPPINGS as ACB_MAP,
-    NODE_DISPLAY_NAME_MAPPINGS as ACB_NAME,
-)
-from .linxutil.levels_match_node import (
-    NODE_CLASS_MAPPINGS as LVM_MAP,
-    NODE_DISPLAY_NAME_MAPPINGS as LVM_NAME,
-)
-from .linxutil.crop_by_margins import (
-    NODE_CLASS_MAPPINGS as CBM_MAP,
-    NODE_DISPLAY_NAME_MAPPINGS as CBM_NAME,
-)
-from .linxutil.filename_append_suffix import (
-    NODE_CLASS_MAPPINGS as FAS_MAP,
-    NODE_DISPLAY_NAME_MAPPINGS as FAS_NAME,
-)
-from .linxutil.stitch_by_mask_feathered import (
-    NODE_CLASS_MAPPINGS as SBM_MAP,
-    NODE_DISPLAY_NAME_MAPPINGS as SBM_NAME,
-)
+# LinxUtil package init (subpackage layout)
+# This imports the modules that actually exist under ./linxutil
+# and merges their NODE_*_MAPPINGS if present.
 
 NODE_CLASS_MAPPINGS = {}
-NODE_CLASS_MAPPINGS.update(MPC_MAP)
-NODE_CLASS_MAPPINGS.update(ACM_MAP)
-NODE_CLASS_MAPPINGS.update(STRIP_EXT_MAP)
-NODE_CLASS_MAPPINGS.update(ACB_MAP)
-NODE_CLASS_MAPPINGS.update(LVM_MAP)
-NODE_CLASS_MAPPINGS.update(CBM_MAP)
-NODE_CLASS_MAPPINGS.update(FAS_MAP)
-NODE_CLASS_MAPPINGS.update(SBM_MAP)
-
 NODE_DISPLAY_NAME_MAPPINGS = {}
-NODE_DISPLAY_NAME_MAPPINGS.update(MPC_NAME)
-NODE_DISPLAY_NAME_MAPPINGS.update(ACM_NAME)
-NODE_DISPLAY_NAME_MAPPINGS.update(STRIP_EXT_NAME)
-NODE_DISPLAY_NAME_MAPPINGS.update(ACB_NAME)
-NODE_DISPLAY_NAME_MAPPINGS.update(LVM_NAME)
-NODE_DISPLAY_NAME_MAPPINGS.update(CBM_NAME)
-NODE_DISPLAY_NAME_MAPPINGS.update(FAS_NAME)
-NODE_DISPLAY_NAME_MAPPINGS.update(SBM_NAME)
+
+def _merge(mod):
+    if hasattr(mod, "NODE_CLASS_MAPPINGS"):
+        NODE_CLASS_MAPPINGS.update(mod.NODE_CLASS_MAPPINGS)
+    if hasattr(mod, "NODE_DISPLAY_NAME_MAPPINGS"):
+        NODE_DISPLAY_NAME_MAPPINGS.update(mod.NODE_DISPLAY_NAME_MAPPINGS)
+
+# Import from the linxutil subpackage using the filenames you actually have
+from .linxutil import multipurpose_configuration as _m_mpc
+_merge(_m_mpc)
+
+from .linxutil import auto_color_match as _m_acm
+_merge(_m_acm)
+
+from .linxutil import remove_extension as _m_strip
+_merge(_m_strip)
+
+from .linxutil import autocrop as _m_autocrop
+_merge(_m_autocrop)
+
+from .linxutil import comfy_ui_levels_match_custom_node as _m_levels
+_merge(_m_levels)
+
+from .linxutil import crop_by_margins as _m_cropm
+_merge(_m_cropm)
+
+from .linxutil import filename_append_suffix as _m_suffix
+_merge(_m_suffix)
+
+from .linxutil import stitch_by_mask as _m_stitch
+_merge(_m_stitch)
+
+print(f"[LinxUtil] Loaded {len(NODE_CLASS_MAPPINGS)} node classes: "
+      + ", ".join(NODE_CLASS_MAPPINGS.keys()))
